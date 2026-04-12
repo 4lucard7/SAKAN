54834dc1cf0f94e65b20343dd3401cf7ee905833
eyouub <ayoubalouhmy02@gmail.com>
2026-04-12 13:23:22 +0100

    add validation logic

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,20 +1,54 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../context/AuthContext.jsx'
-import { User, Mail, Lock, Eye, EyeOff } from 'lucide-react'
+import { User, Mail, Lock, Eye, EyeOff,AlertCircle } from 'lucide-react'
 
 export default function RegisterPage() {
   const { register, loading } = useAuth()
   const navigate = useNavigate()
   const [form, setForm] = useState({ name: '', email: '', password: '', password_confirmation: '' })
   const [show, setShow] = useState(false)
-  const [error, setError] = useState(null)
+  const [error, setError] = useState("")
+  const [emailError, setEmailError] = useState(false)
+  const [pwdError, setPwdError] = useState(false)
+  const [nameError, setNameError] = useState(false)
+  const [match_pwd_error, setMatch_pwd_error] = useState(false)
 
-  const handle = (e) => setForm(f => ({ ...f, [e.target.name]: e.target.value }))
+
+
+
+  const handle = (e) => {
+    const {name,value}=e.target;
+    setForm(f => ({ ...f, [name]:value }))
+    if(name ==="email"){
+      setEmailError(value.length > 0 && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value))
+    }
+    if(name ==="name"){
+      setNameError(value.trim()==="")
+    }
+    if (name === "password") {
+      setPasswordError(value.trim() === "")
+    }
+
+  }
 
   const submit = async (e) => {
     e.preventDefault()
-    setError(null)
+    const isvalidEmail=/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(form.email);
+    setEmailError(!isvalidEmail);
+    const isNameEmpty=form.name.trim()==="";
+    setNameError(isNameEmpty);
+    const isvalidPassword=form.password.trim() !== "";
+    setPwdError(!isvalidPassword)
+    if(isNameEmpty || !isvalidEmail ||!isvalidPassword) return;
+    if(form.password !== form.password_confirmation){
+      setMatch_pwd_error(true)
+      return;
+      
+    }else{
+      setMatch_pwd_error(false)
+    }
+
     const res = await register(form)
     if (res.success) {
       navigate('/dashboard')
@@ -45,52 +79,77 @@ export default function RegisterPage() {
           <span className="text-[#2196F3] font-extrabold text-xl tracking-widest uppercase">Sakan</span>
         </div>
 
-        {error && (
-          <div className="rounded-xl bg-red-50 border border-red-200 text-red-700 px-4 py-3 mb-4 text-sm">
-            {error}
-          </div>
-        )}
+        
 
           <form onSubmit={submit} className="flex flex-col gap-4">
             <div>
               <label className="label">nom <span className="text-red-500">*</span></label>
               <div className="relative">
                 <User size={15} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-                <input name="name" required value={form.name} onChange={handle}
+                <input name="name"  value={form.name} onChange={handle}
                   placeholder="Votre nom complet" className="input pl-9" />
               </div>
+              {nameError &&(
+                <div className="flex items-center gap-1.5 mt-0.5">
+                <AlertCircle className="w-3.5 h-3.5 text-red-500 shrink-0" />
+                <span className="text-xs text-red-500">Veuillez entrer un nom valide.</span>
+              </div>
+              )}
             </div>
 
             <div>
               <label className="label">email <span className="text-red-500">*</span></label>
               <div className="relative">
                 <Mail size={15} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-                <input name="email" type="email" required value={form.email} onChange={handle}
+                <input name="email" type="email"  value={form.email} onChange={handle}
                   placeholder="name@example.com" className="input pl-9" />
               </div>
+              {emailError && (
+                <div className="flex items-center gap-1.5 mt-0.5">
+                <AlertCircle className="w-3.5 h-3.5 text-red-500 shrink-0" />
+                <span className="text-xs text-red-500">Veuillez saisir une addresse email valide.</span>
+              </div>
+                
+              )}
             </div>
 
             <div>
               <label className="label">Mot de passe <span className="text-red-500">*</span></label>
               <div className="relative">
                 <Lock size={15} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-                <input name="password" type={show ? 'text' : 'password'} required
+                <input name="password" type={show ? 'text' : 'password'} 
                   value={form.password} onChange={handle} placeholder="••••••••" className="input pl-9 pr-10" />
                 <button type="button" onClick={() => setShow(s => !s)}
                   className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
                   {show ? <EyeOff size={15} /> : <Eye size={15} />}
                 </button>
               </div>
+              {pwdError && (
+                <div className="flex items-center gap-1.5 mt-0.5">
+                  <AlertCircle className="w-3.5 h-3.5 text-red-500 shrink-0" />
+                  <span className="text-xs text-red-500">
+                    Saisie un mot de passe de 8 caractére
+                  </span>
+                </div>
+               )}
             </div>
 
             <div>
               <label className="label">Confirmer le mot de passe <span className="text-red-500">*</span></label>
               <div className="relative">
                 <Lock size={15} className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
-                <input name="password_confirmation" type={show ? 'text' : 'password'} required
+                <input name="password_confirmation" type={show ? 'text' : 'password'} 
                   value={form.password_confirmation} onChange={handle}
                   placeholder="••••••••" className="input pl-9" />
               </div>
+              {match_pwd_error && (
+                <div className="flex items-center gap-1.5 mt-0.5">
+                  <AlertCircle className="w-3.5 h-3.5 text-red-500 shrink-0" />
+                  <span className="text-xs text-red-500">
+                    Les mots de passe ne correspondent pas
+                  </span>
+                </div>
+              )}
             </div>
 
             <button type="submit" disabled={loading} className="bg-[#2196F3] justify-center text-white font-semibold  rounded-xl py-2 hover:bg-[#1976D2] transition">
