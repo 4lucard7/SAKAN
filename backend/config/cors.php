3d42c7cc20103b1e0f5b6121c3389cd6d65726e2
eyouub <ayoubalouhmy02@gmail.com>
2026-05-02 20:48:47 +0100

    add configuration

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -21,7 +21,8 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:5174',
-        'http://127.0.0.1:5174'
+        'http://127.0.0.1:5174',
+        // 'https://relapsing-disprove-maroon.ngrok-free.dev' "lien pour tester via ngrok "
     ],
 
     'allowed_origins_patterns' => [],
